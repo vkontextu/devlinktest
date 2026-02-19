f2858ceb6892b2a49dcebc670a68562b6615f439
vkontextu <166528469+vkontextu@users.noreply.github.com>
2026-02-19 23:53:51 +0100

    Webflow CLI setup

---
+++ node_modules/webflow-api/api/resources/assets/client/requests/index.d.ts
@@ -0,0 +1,3 @@
+export { type AssetsCreateRequest } from "./AssetsCreateRequest";
+export { type AssetsUpdateRequest } from "./AssetsUpdateRequest";
+export { type AssetsCreateFolderRequest } from "./AssetsCreateFolderRequest";
